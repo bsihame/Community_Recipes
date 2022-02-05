27438a8ea6b4524adf015d8233fecf6da05fa731
bsihame <bsihame@hotmail.com>
2022-02-04 22:29:36 -0500

    add video folder with mp4 video

--- Frontend/src/components/home/Home.js
+++ Frontend/src/components/home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Footer }from "../headerFooter/Footer";
+
 export const Home = () => {
 	return (
 		<>
 			<div className="homeDiv">
-			this is home page
+			<div className="box "></div>
 			</div>
-			<div>
-				<Footer className="homeFooter"/>
-			</div>
+		
 		</>
 	);
 }
